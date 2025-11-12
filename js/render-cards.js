54cfcac315e7fbef7ef2e4ab17f0e4277dcb0bb0
DANILA GANSHIN <125364917+Cooper-Farnsworth@users.noreply.github.com>
2025-11-12 18:47:28 +0300

    почти доделали, осталось доработать listener

--- js/render-cards.js
+++ js/render-cards.js
@@ -1,8 +1,9 @@
+import { openModal } from './modal-window.js';
+
 const cardTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesContainerNode = document.querySelector('.pictures');
 
 export const renderPhotos = (photos) => {
-  console.log(photos);
   const fragment = document.createDocumentFragment();
   photos.forEach(({url, description, comments, likes}) => {
     const newCard = cardTemplate.cloneNode(true);
@@ -11,6 +12,9 @@ export const renderPhotos = (photos) => {
     image.alt = description;
     newCard.querySelector('.picture__comments').textContent = comments.length;
     newCard.querySelector('.picture__likes').textContent = likes;
+    newCard.addEventListener('click', () => {
+      openModal({url, description, comments, likes});
+    });
     fragment.append(newCard);
   });
   picturesContainerNode.append(fragment);
